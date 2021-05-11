132e7184bf97f88f980f4377ab8250c80f200c06
taylor-ann <80003984+taylor-ann@users.noreply.github.com>
2021-05-11 00:12:40 -0400

    fixed missing ampersand

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ forecastHTML +
 
 function getForecast(coordinates){
 let apiKey = "c6c31d723664fecd685ff24479371099";
-let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayForecast);
 }
 
